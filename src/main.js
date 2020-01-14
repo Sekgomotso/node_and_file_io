7d912e72d212b5c08453f71d0252b042c0ab50de
Sekgomotso <motsoshalang@gmail.com>
2020-01-14 16:26:41 +0200

    save

---
+++ src/main.js
@@ -0,0 +1,21 @@
+let fs = require('fs');
+
+class Visitor {
+    constructor(full_name, Age, Date, Time,Comments, Assistant) {
+        this.full_name = full_name;
+        this.age = Age;
+        this.dateOfVisit = Date;
+        this.timeOfVisit = Time;
+        this.comments = Comments;
+        this.assistant = Assistant;
+    }
+
+    async save() {
+        fs.appendFile('visitor_{motso}.json'), JSON.singify(this.full_name), function (err) {
+            if (err) throw err;
+            console.log('done')
+        }
+    }
+}
+
+motso.save();

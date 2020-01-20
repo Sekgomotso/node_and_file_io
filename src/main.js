9349ab81b15a937ae3b6016b219d788ae683536d
Sekgomotso <motsoshalang@gmail.com>
2020-01-20 12:43:40 +0200

    progress

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+"use strict"
+
 let fs = require('fs');
 
 class Visitor {
-    constructor(full_name, Age, Date, Time,Comments, Assistant) {
+    constructor(full_name, Age, Date, Time, Comments, Assistant) {
         this.full_name = full_name;
         this.age = Age;
         this.dateOfVisit = Date;
@@ -11,25 +13,13 @@ class Visitor {
     }
 
     async save() {
-        fs.appendFile('visitor_{full_name}.json'), JSON.singify(this.full_name), function (err) {
+        fs.appendFile('visitor_{their_full_name}.json', JSON.stringify(this.fullName), (err)=>{
             if (err) throw err;
-            console.log('done')
-        }
+            console.log('done');
+        });
     }
 }
 
-// async load(name) {
-//     const fs  = require('fs').promises
-//     let data = JSON.stringify(this)
-//     try {
-//         let query = await fs.writeFile{./Workspace$(this.full_name)}.json.data
-//     }
-// }
-
-var motso = new Visitor("motso",30,"17/09/2020","00:00am","Happy_birthday","Raymond");
-// var thato = new Visitor("thato",23,"12/09/2020","12:00pm","Heey","juju");
+let motso = new Visitor("motso",30,"17/09/2020","00:00am","happy_birthday", "raymond");
 
 motso.save();
-// thato.save();
-
-module.exports = { Visitor }

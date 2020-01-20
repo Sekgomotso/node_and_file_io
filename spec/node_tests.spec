7c7c61bfc475139d9cd1c5e2da04c12f620b5eff
Sekgomotso <motsoshalang@gmail.com>
2020-01-20 11:58:12 +0200

    test one

---
+++ spec/node_tests.spec
@@ -0,0 +1,8 @@
+var {Visitor} = require('..src/main.js');
+var motso = new Visitor;
+
+describe("function that saves visitor's data", () => {
+    it("should return visitor's full name", ()=>{
+        expect("motso.save()").toBe(visitor_motso.json)
+    })
+})
--- src/main.js
+++ src/main.js
@@ -11,18 +11,25 @@ class Visitor {
     }
 
     async save() {
-        fs.appendFile('visitor_{motso}.json'), JSON.singify(this.full_name), function (err) {
+        fs.appendFile('visitor_{full_name}.json'), JSON.singify(this.full_name), function (err) {
             if (err) throw err;
             console.log('done')
         }
     }
 }
 
-async load(name) {
-    const fs  = require('fs').promises
-    let data = JSON.stringify(this)
-    try {
-        let query = await fs.writeFile{./Workspace$(this.full_name)}.json.data
-    }
-}
+// async load(name) {
+//     const fs  = require('fs').promises
+//     let data = JSON.stringify(this)
+//     try {
+//         let query = await fs.writeFile{./Workspace$(this.full_name)}.json.data
+//     }
+// }
+
+var motso = new Visitor("motso",30,"17/09/2020","00:00am","Happy_birthday","Raymond");
+// var thato = new Visitor("thato",23,"12/09/2020","12:00pm","Heey","juju");
+
 motso.save();
+// thato.save();
+
+module.exports = { Visitor }

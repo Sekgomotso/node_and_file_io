00511e3ada5ccfe3ee9c646dab116333307b659d
Sekgomotso <motsoshalang@gmail.com>
2020-02-04 11:08:06 +0200

    working on it

--- spec/visitor.spec.js
+++ spec/visitor.spec.js
@@ -1,42 +1,43 @@
+let fs = require('fs');
 const {Visitor} = require('../src/main');
 
+let motso = {
+    full_name: "sekgomotso shalang",
+    Age: 30,
+    Date: "17/09/2020",
+    Time: "00:00",
+    Comments: "Happy birthday",
+    Assistant: "Tumi"
+}
+
+let sekgomotso = new Visitor (
+    motso.full_name,
+    motso.Age,
+    motso.Date,
+    motso.Time,
+    motso.Comments,
+    motso.Assistant
+);
+
 describe("function that saves visitors data", ()=>{
-    let sekgomotso;
-    let sekgomotsoLoad;
-    beforeEach(()=>{
-        let sekgomotso = new Visitor("sekgomotso shalang",30,"17/09/2020","00:00","Happy birthday","Tumi");
-        sekgomotso = sekgomotso;
-
-        let fs = require('fs');
-
-        let full_name = sekgomotso.full_name;
-
-        let name = full_name.replace(' ','_').toLowerCase();
-
-        let load = fs.readFile(`visitor_${name}.json`, 'utf8', (err, data)=>{
-            if(err) throw err;
-            console.log(data);
-        });
-
-        sekgomotsoLoad = JSON.parse(load("sekgomotso shalang"))
-    });
 
     it("should return visitor's full name", ()=>{
-        expect(sekgomotso).toBe(sekgomotsoLoad);
+        expect(sekgomotso.full_name).toBe(motso.full_name);
     })
-})
+});
 
 describe("function that takes name and grabs visitor's data", ()=>{
     let sekgomotso;
 
     beforeEach(()=>{
-        let name = full_name.replace(' ','_').toLowerCase();
+        let name = motso.full_name.replace(' ','_').toLowerCase();
 
         let load = fs.readFile(`visitor_${name}.json`, 'utf8', (err, data)=>{
             if(err) throw err;
             console.log(data);
         });
-        sekgomotsoLoad = JSON.parse(load("sekgomotso shalang"))
+    
+        sekgomotsoLoad = JSON.parse(load("Sekgomotso shalang"))
     });
     it("should return age", ()=>{
         expect(sekgomotso.Age).toBe(sekgomotsoLoad.Age);
